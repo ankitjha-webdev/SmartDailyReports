15c435a2c54bafda669ad5d7b555054d4d375d2a
ANKIT KUMAR <190101120091@cutm.ac.in>
2024-03-07 15:59:45 +0530

    chanegs made

--- src/components/report/component.tsx
+++ src/components/report/component.tsx
@@ -5,11 +5,13 @@ export function Report() {
     const [text, setText] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
+    const [hasError, setHasError] = useState(false);
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         try {
             e.preventDefault();
             if (!text.trim()) {
+                setHasError(true);
                 setError('Oops! Looks like you forgot to fill in this field.')
                 return;
             }
@@ -45,7 +47,7 @@ export function Report() {
                     <textarea value={text} onChange={(e) => {
                         setError('');
                         return setText(e.target.value);
-                    }} className={`block w-full h-24 p-2 mt-1 border-2 border-gray-300 rounded-md form-textarea ${error ? 'border-red-600' : ''}`} placeholder="Share your polished or incomplete sentense"></textarea>
+                    }} className={`block w-full h-24 p-2 mt-1 border-2 border-gray-300 rounded-md form-textarea ${error && hasError ? 'border-red-600' : ''}`} placeholder="Share your polished or incomplete sentense"></textarea>
                     {error && <span className="text-red-600">{error}</span>}
                     <div className="flex items-center justify-center">
                         <button type="submit" className={`flex justify-center items-center w-full shadow px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-800 transition ease-in-out duration-150 ${loading ? 'cursor-not-allowed opacity-50' : ''}`} disabled={loading}>
